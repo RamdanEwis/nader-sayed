c2c454f9eaaa2884ff1cb8cd9e87d4db2d489d57
RamadanEwis <43334754+RamdanEwis@users.noreply.github.com>
2023-03-16 04:40:06 -0700

    Update emailkey.js

--- src/components/emailkey.js
+++ src/components/emailkey.js
@@ -1,6 +1,6 @@
 // eslint-disable-next-line import/no-anonymous-default-export
 export default {
- //   USER_ID: `xCNspFvG99I3kQBhR`,
-  //  TEMPLATE_ID: `template_3vxhk2f`,
-  //  SERVICE_ID: "service_ojs52ff"
+  USER_ID: `xCNspFvG99I3kQBhR`,
+   TEMPLATE_ID: `template_3vxhk2f`,
+   SERVICE_ID: "service_ojs52ff"
 }
